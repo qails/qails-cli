35344e1545acc6c2fdd5d3484990c03d06592cc2
zhongzhi107 <zhong.zhi@163.com>
2017-09-26 18:17:01 +0800

    chore: Update init templates

--- templates/init/src/config/knexfile.js
+++ templates/init/src/config/knexfile.js
@@ -2,15 +2,16 @@
  * 该文件仅供 knex-cli 使用
  * model 中的数据库配置信息是直接从 .env 中获取
  */
-require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({ path: '../.env' });
 
 const {
-  KNEX_CLIENT = 'mysql',
-  MYSQL_HOST = 'localhost',
-  MYSQL_USER = 'root',
-  MYSQL_PASSWORD = '',
-  MYSQL_DATABASE = 'qrmaps',
-  MYSQL_PORT = '3306'
+  KNEX_CLIENT,
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+  MYSQL_PORT,
+  NODE_ENV
 } = process.env;
 
 module.exports = {
@@ -20,13 +21,15 @@ module.exports = {
     user: MYSQL_USER,
     password: MYSQL_PASSWORD,
     database: MYSQL_DATABASE,
-    port: MYSQL_PORT
+    port: MYSQL_PORT,
+    charset: 'utf8',
+    debug: NODE_ENV === 'local'
   },
   migrations: {
     tableName: 'migrations',
-    directory: '../../migrations'
+    directory: '../migrations'
   },
   seeds: {
-    directory: '../../seeds'
+    directory: '../seeds'
   }
 };
