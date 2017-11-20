afecff4055d9ee78f0f6bdc1c9840eb3e22a4347
zhongzhi107 <zhong.zhi@163.com>
2017-11-20 16:11:13 +0800

    feat: 使用node@^8.9.1并使用npm代替yarn

--- templates/init/scripts/build.sh
+++ templates/init/scripts/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# 编译机上可用node版本：[0.12.7/4.2.4/5.12.0/6.2.1/6.9.2/7.10.0/8.9.1]
 # export PATH=/usr/local/n/versions/node/6.2.1/bin:$PATH
-export PATH=/usr/local/n/versions/node/7.10.0/bin:$PATH
+# export PATH=/usr/local/n/versions/node/7.10.0/bin:$PATH
+export PATH=/usr/local/n/versions/node/8.9.1/bin:$PATH
 
 echo "[build.sh] hostname: `hostname`"
 echo "[build.sh] node version: `node -v`"
@@ -14,6 +16,6 @@ ls -la
 
 cp profiles/.env.${NODE_ENV} .env && echo '.env copied'
 
-yarn && yarn build
+npm install && npm run build
 
 ls -la
